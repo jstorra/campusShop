72567765e881a2d9500198fbefcf190f8dae608c
jstorra <131458634+jstorra@users.noreply.github.com>
2023-10-13 13:29:15 -0500

    feat: :sparkles: Se agrega codigo
    Se añade codigo en el archivo app.js el cual sera el archivo de arranque de toda la aplicación.

---
+++ app.js
@@ -0,0 +1,15 @@
+import m from "./modules/loadMains.js";
+const content = document.querySelector("#content");
+
+addEventListener("DOMContentLoaded", async () => {
+    let currentPage = "todos"
+    m[`${currentPage}Main`](content)
+    const pages = document.querySelectorAll("a[id]")
+    pages.forEach(btn => {
+        btn.addEventListener("click", () => {
+            document.querySelector("main").remove();
+            currentPage = btn.getAttribute("id").substring(3).toLowerCase()
+            m[`${currentPage}Main`](content)
+        })
+    })
+});
